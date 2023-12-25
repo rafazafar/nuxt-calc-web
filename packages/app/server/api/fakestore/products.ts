08ca5520c85f1bc4616935bf505ad9d0ab22108d
ahmad.zafar <ahmad.zafar@axa.co.jp>
2023-12-25 11:32:43 +0900

    cleanup

--- packages/app/server/api/fakestore/products.ts
+++ packages/app/server/api/fakestore/products.ts
@@ -1,5 +1,3 @@
-import { FakestoreProduct } from "~/composables/useTypes"
-
 export default defineEventHandler(async (event) => {
   const res = await fetch('https://fakestoreapi.com/products')
   return await res.json()
