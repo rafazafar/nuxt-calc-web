833320238d78c8ed032d5d9b92490fbcff7030b6
ahmad.zafar <ahmad.zafar@axa.co.jp>
2023-12-29 15:17:30 +0900

    await data

--- packages/app/pages/achievements.vue
+++ packages/app/pages/achievements.vue
@@ -3,12 +3,10 @@
     <PageTitle :title="pageTitle" :description="pageDescription" />
     <p class="font-semibold text-xl">支給年月: {{ "2022年3月" }}</p>
     <div class="overflow-x-auto"></div>
-    <ClientOnly>
-      <h2 class="text-xl mt-4">ElementPlus Table:</h2>
-      <TableAchievementsElementTable />
-      <h2 class="text-xl mt-4">React Table:</h2>
-      <TableAchievementsReactTable :table-data="first5" />
-    </ClientOnly>
+    <h2 class="text-xl mt-4">ElementPlus Table:</h2>
+    <TableAchievementsElementTable />
+    <h2 class="text-xl mt-4">React Table:</h2>
+    <TableAchievementsReactTable :table-data="first5" />
   </div>
 </template>
 
@@ -16,7 +14,7 @@
 const pageTitle = "支給年月別実績一覧照会(営業所長)";
 const pageDescription = "";
 
-const { data, pending, error, refresh, status } = useFetch<any>(
+const { data, pending, error, refresh, status } = await useFetch<any>(
   "/api/achievements",
   { key: "achievementsData" }
 );
